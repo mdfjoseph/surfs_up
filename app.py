f5e62ceccdd90c60dd0e049f97a4591ebaba0ca5
Marcia Joseph <mdfugh@yahoo.com>
2023-03-08 14:09:26 -0600

    Module_9.3.4_Update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello world'
+    return 'Hello world. Next add /test to the URL.'
 
-
+@app.route('/test')
+def test_page():
+    return "This is your test page"
